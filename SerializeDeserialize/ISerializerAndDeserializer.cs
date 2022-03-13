83667f56713229439701a6b2fd9056549e90e78e
峻也 本田 <morlyckbd@gmail.com>
2022-03-13 23:21:56 +0900

    名前空間の変更

--- SerializeDeserialize/ISerializerAndDeserializer.cs
+++ SerializeDeserialize/ISerializerAndDeserializer.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CommonElement.SerializeDeserialize
+namespace CommonElement
 {
     public interface ISerializerAndDeserializer
     {
