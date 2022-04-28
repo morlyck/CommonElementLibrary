47588192db07d25e2eb932b9fb69b57ca1cbc266
峻也 本田 <morlyckbd@gmail.com>
2022-04-28 11:44:02 +0900

    インターフェースの実装

--- ChainEnvironment/v0_0_1/ChainEnvironment_v0_0_1.cs
+++ ChainEnvironment/v0_0_1/ChainEnvironment_v0_0_1.cs
@@ -6,7 +6,102 @@ using System.Threading.Tasks;
 
 namespace CommonElement.ChainEnvironment_v0_0_1
 {
-    public class ChainEnvironment
+    public class ChainEnvironment : IChainEnvironment
     {
+        public string Version { get; } = "0.0.1";
+
+        public bool MultiBand => throw new NotImplementedException();
+
+        public void ClearUpstairEnvironmentSetting() {
+            throw new NotImplementedException();
+        }
+
+        public DataType CreateOrSetValue_Local<DataType>(string variableName, object value) {
+            throw new NotImplementedException();
+        }
+
+        public object CreateOrSetValue_Local(Type type, string variableName, object value) {
+            throw new NotImplementedException();
+        }
+
+        public void Deserialize(ISerializerAndDeserializer deserializer, string text) {
+            throw new NotImplementedException();
+        }
+
+        public void Down() {
+            throw new NotImplementedException();
+        }
+
+        public void Down(List<(Type, string)> returnValues, List<(Type, string)> arguments) {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists<DataType>(string variableName) {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(Type type, string variableName) {
+            throw new NotImplementedException();
+        }
+
+        public DataType GetValue<DataType>(string variableName) {
+            throw new NotImplementedException();
+        }
+
+        public object GetValue(Type type, string variableName) {
+            throw new NotImplementedException();
+        }
+
+        public void PullArguments() {
+            throw new NotImplementedException();
+        }
+
+        public void PullArguments(List<(Type, string)> variables) {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove<DataType>(string variableName) {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Type type, string variableName) {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveAll<DataType>(string variableName) {
+            throw new NotImplementedException();
+        }
+
+        public string Serialize(ISerializerAndDeserializer serializer) {
+            throw new NotImplementedException();
+        }
+
+        public void SetUpstairEnvironment(IUpstairEnvironment upstairEnvironment, int connectionFloorNo) {
+            throw new NotImplementedException();
+        }
+
+        public void SetUpstairEnvironment_ConnectionToCurrentFloorNo(IUpstairEnvironment upstairEnvironment) {
+            throw new NotImplementedException();
+        }
+
+        public void SetUpstairEnvironment_LooseConnection(IUpstairEnvironment upstairEnvironment) {
+            throw new NotImplementedException();
+        }
+
+        public DataType SetValue<DataType>(string variableName, object value) {
+            throw new NotImplementedException();
+        }
+
+        public object SetValue(Type type, string variableName, object value) {
+            throw new NotImplementedException();
+        }
+
+        public void Up() {
+            throw new NotImplementedException();
+        }
+
+        public void Up(List<(Type, string)> returnValues) {
+            throw new NotImplementedException();
+        }
     }
 }
