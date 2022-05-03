49a0c8c28093b89e058234af89d5f9d2c9c34910
峻也 本田 <morlyckbd@gmail.com>
2022-05-04 00:08:27 +0900

    インターフェースの実装

--- ChainEnvironment/Common/IUpstairChain.cs
+++ ChainEnvironment/Common/IUpstairChain.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace CommonElement
 {
-    public class IUpstairChain
+    public interface IUpstairChain
     {
+        object GetValue(Type type, string variableName);
+        object SetValue(Type type, string variableName, object value);
+        object CreateOrSetValue_Local(Type type, string variableName, object value);
+        bool Exists(Type type, string variableName);
+        bool Remove(Type type, string variableName);
+        bool RemoveAll(Type type, string variableName);
     }
 }
