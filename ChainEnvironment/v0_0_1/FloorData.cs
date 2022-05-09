0f49c33e92f9c6186ea1057b26dd225d6946c889
峻也 本田 <morlyckbd@gmail.com>
2022-05-10 00:59:24 +0900

    FloorDataの追加

---
+++ ChainEnvironment/v0_0_1/FloorData.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonElement.ChainEnvironment_v0_0_1
+{
+    internal class FloorData
+    {
+
+    }
+}
