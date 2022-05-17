4181f49c80383bcf1eeea87fe40a1501ba53299e
峻也 本田 <morlyckbd@gmail.com>
2022-05-18 00:02:01 +0900

    IChainEnvironmentExの実装

--- ChainEnvironment/Common/IChainEnvironmentEx.cs
+++ ChainEnvironment/Common/IChainEnvironmentEx.cs
@@ -9,35 +9,32 @@ namespace CommonElement
     public static class IChainEnvironmentEx
     {
         public static DataType GetValue<DataType>(this IChainEnvironment env, string variableName) {
-            throw new NotImplementedException();
+            env.TryGetValue(typeof(DataType), variableName, out object value);
+            return (DataType)value;
         }
-        public static bool TryGetValue<DataType>(this IChainEnvironment env, string variableName, out object value) {
-            throw new NotImplementedException();
+        public static bool TryGetValue<DataType>(this IChainEnvironment env, string variableName, out DataType value) {
+            var result = env.TryGetValue(typeof(DataType), variableName, out object temp);
+            value = (DataType)temp;
+            return result;
         }
-        public static DataType SetValue<DataType>(this IChainEnvironment env, string variableName, object value) {
-            throw new NotImplementedException();
+        public static DataType SetValue<DataType>(this IChainEnvironment env, string variableName, DataType value) {
+            env.TrySetValue(typeof(DataType), variableName, value);
+            return value;
         }
-        public static DataType CreateOrSetValue_Locally<DataType>(this IChainEnvironment env, string variableName, object value) {
-            throw new NotImplementedException();
+        public static DataType CreateOrSetValue_Locally<DataType>(this IChainEnvironment env, string variableName, DataType value) {
+            env.TryCreateOrSetValue_Locally(typeof(DataType), variableName, value);
+            return value;
         }
         public static bool Exists<DataType>(this IChainEnvironment env, string variableName) {
-            throw new NotImplementedException();
+            return env.Exists(typeof(DataType), variableName);
         }
         public static bool Remove<DataType>(this IChainEnvironment env, string variableName) {
-            throw new NotImplementedException();
-        }
-        public static void RemoveAll<DataType>(this IChainEnvironment env, string variableName) {
-            throw new NotImplementedException();
+            return env.Remove(typeof(DataType), variableName);
         }
         //
-        public static object GetValue(Type type, string variableName) {
-            throw new NotImplementedException();
-        }
-        public static DataType SetValue<DataType>(this IChainEnvironment env, Type type, string variableName, object value) {
-            throw new NotImplementedException();
-        }
-        public static DataType CreateOrSetValue_Locally<DataType>(this IChainEnvironment env, Type type, string variableName, object value) {
-            throw new NotImplementedException();
+        public static object GetValue(this IChainEnvironment env, Type type, string variableName) {
+            env.TryGetValue(type, variableName, out object value);
+            return value;
         }
     }
 }
